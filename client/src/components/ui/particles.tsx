4fe57ddf6645cecb63bd5561a9259a618a0d3ce9
Abdirashiid Sammantar <abdirashiidsammantar@example.com>
2026-01-06 23:43:36 +0100

    fix(build): rename Particles.css to particles.css to fix build error

--- client/src/components/ui/particles.tsx
+++ client/src/components/ui/particles.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react';
 import { Renderer, Camera, Geometry, Program, Mesh } from 'ogl';
 
-import './Particles.css';
+import './particles.css';
 
 interface ParticlesProps {
     particleCount?: number;
